36d3d5aeff22013ed306e4147ab3724c4c5fcfa0
samsonmorara-maker/samsongit config  --global user.email samson.morara@student.moringaschool.com <samson.morara@student.moringaschool.com>
2026-04-29 17:53:10 +0300

    first changes

---
+++ React-learning/src/component/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <h1>My Website</h1>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/contact">Contact</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
