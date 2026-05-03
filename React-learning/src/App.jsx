824bb0446fe6cb61aa81631d3a64572316c56c6a
samsonmorara-maker/samsongit config  --global user.email samson.morara@student.moringaschool.com <samson.morara@student.moringaschool.com>
2026-05-03 09:26:07 +0300

    confirmation of tailwindcss working

--- React-learning/src/App.jsx
+++ React-learning/src/App.jsx
@@ -1,16 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import './index.css'
 import './App.css'
 import Navbar from './component/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-  
+      <Navbar />
     </>
   )
 }
